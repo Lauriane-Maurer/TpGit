d26555c5cb78216a75b4b1d6a2a3103581031c07
LMaurer <121942116+Lauriane-Maurer@users.noreply.github.com>
2023-03-21 16:40:14 +0100

    Ajout boucle while pour redemander choix tant que non valide

--- src/App.java
+++ src/App.java
@@ -1,9 +1,10 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
-        int choix = 1;
+        int choix = 0;
 
         Anglais vAnglais = new Anglais();
         vAnglais.addDico("Bonjour", "Hello");
@@ -14,16 +15,18 @@ public class App {
         System.out.println("1 : Anglais");
         System.out.println("2 : Breton");
 
-        choix = scan.nextInt();
-
-        if(choix == 1){
-            System.out.println(vAnglais.getTraduction("Bonjour"));
-        }else{
-            System.out.println(vBreton.getTraduction("Bonjour"));
+        while (choix!=1 && choix !=2) {
+            choix = scan.nextInt();
+        if (choix != 1 && choix != 2) {
+            System.out.println("Veuillez saisir une valeur valide (1 ou 2) :");
         }
-
-
+    }
+            if (choix == 1) {
+                System.out.println(vAnglais.getTraduction("Bonjour"));
+            } else if (choix == 2) {
+            System.out.println(vBreton.getTraduction("Bonjour"));
+            }
+    }
         //Fenetre f = new Fenetre();
         //f.setVisible(true);
-    }
 }
