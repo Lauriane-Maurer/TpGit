5ddef398e5130cb37a1f9c009b0136efb83974a1
LMaurer <121942116+Lauriane-Maurer@users.noreply.github.com>
2023-03-21 16:07:48 +0100

    Ajout classes app, breton, version et anglais

---
+++ src/Anglais.java
@@ -0,0 +1,12 @@
+    public class Anglais extends Version {
+
+        public Anglais(){
+            super();
+            this.entete = "En anglais : ";
+        }
+
+        public String getTraduction(String enFrancais){
+            return this.entete + this.dico.get(enFrancais);
+        }
+    }
+
